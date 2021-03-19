e3f181abb10623ae0e7965e59017e5c5e3a41a25
Borja Zarco <borja@squaads.com>
2021-03-19 13:02:15 +0000

    update mongoose methods

--- src/repositories/mongoose/mongoose.repository.ts
+++ src/repositories/mongoose/mongoose.repository.ts
@@ -1,14 +1,23 @@
-import { Document, NativeError, Query, QueryPopulateOptions, Schema, Types } from 'mongoose';
+import { Query, QueryPopulateOptions, Schema, Types } from 'mongoose';
 import BaseRepository from '../../base/base.repository';
-import mongooseConnector from './mongoose-connector.service';
+import mongooseConnector, { MONGOOSE_MODELS } from './mongoose-connector.service';
 
 export default class MongooseRepo implements BaseRepository {
   collection: string;
   schema: Schema<any>;
 
-  constructor(collection: string, schema: Schema) {
+  constructor(collection: string, schema?: Schema) {
     this.collection = collection;
-    this.schema = schema;
+    this.schema = schema || this.getSchema(collection);
+  }
+
+  private getSchema(entity: string): Schema {
+    const schema = MONGOOSE_MODELS.find((model) => model.name === entity)?.schema;
+    if (!schema) {
+      throw new Error('No schema found!');
+    }
+
+    return schema;
   }
 
   private async createModel() {
@@ -30,57 +39,45 @@ export default class MongooseRepo implements BaseRepository {
     projection?: Object,
     options?: Object,
     populateFields?: string[] | QueryPopulateOptions[]
-  ): Promise<{ data: Document[]; count: number }> {
-    const model = await this.createModel();
-    const result = await new Promise<{ data: Document[]; count: number }>(async (resolve) => {
-      await model
-        .find(filter, projection, options)
-        .populate(populateFields)
-        .exec(async (err, data) => {
-          if (data.length > 0) {
-            return await model.count({}).exec((err: NativeError, count: number) => resolve({ data, count }));
-          }
-        });
-    });
-    return result;
+  ): Promise<any[]> {
+    const Model = await this.createModel();
+    return Model.find(filter, projection, options).populate(populateFields).exec();
+  }
+
+  async getSubItems(itemId: string, filter: Record<string, unknown> = {}): Promise<any[]> {
+    const Model = await this.createModel();
+    return Model.find({ _id: itemId }, filter).exec();
   }
 
   async getById(_id: string, populateFields?: string[] | QueryPopulateOptions[]): Promise<any> {
-    const model = await this.createModel();
-    return model.findById(_id).populate(populateFields).exec();
-  }
-
-  async getOne(filter?: Object, populateFields?: string[] | QueryPopulateOptions[]): Promise<any> {
-    const model = await this.createModel();
-    return model.findOne(filter).populate(populateFields).exec();
-  }
-
-  populate(query: Query<any>, fieldsToPopulate: string[] | QueryPopulateOptions[]): Query<any> {
-    return query.populate(fieldsToPopulate);
-  }
-
-  async delete(_id: string): Promise<any | null> {
-    const model = await this.createModel();
-    return model.findByIdAndDelete(_id).exec();
+    const Model = await this.createModel();
+    return Model.findById(_id).populate(populateFields).exec();
   }
 
   async create(newDocument: any): Promise<any> {
-    const model = await this.createModel();
-    return model.create(newDocument);
+    const Model = await this.createModel();
+    return Model.create(newDocument);
   }
 
-  createMany(newDocument: any[]): Promise<any> {
-    return this.createMany(newDocument);
+  async createMany(newDocument: any): Promise<any> {
+    const Model = await this.createModel();
+    return Model.insertMany(newDocument);
   }
 
   async update(_id: string, newDocument: any): Promise<any | null> {
-    const model = await this.createModel();
-    return model
-      .findByIdAndUpdate(_id, newDocument, {
-        new: true,
-        runValidators: true,
-      })
-      .exec();
+    const Model = await this.createModel();
+    return Model.findByIdAndUpdate(_id, newDocument, {
+      new: true,
+      runValidators: true,
+    }).exec();
+  }
+
+  async updateMany(query: any, newDocument: any): Promise<any | null> {
+    const Model = await this.createModel();
+    return Model.updateMany(query, newDocument, {
+      new: true,
+      runValidators: true,
+    }).exec();
   }
 
   async addArrayElement(id: any, propName: string, newElement: any): Promise<any | null> {
@@ -128,29 +125,45 @@ export default class MongooseRepo implements BaseRepository {
     ).exec();
   }
 
-  async findOneAndUpdate(filter: Object, newDocument: any): Promise<any | null> {
-    const model = await this.createModel();
-    return model
-      .findOneAndUpdate(filter, newDocument, {
-        new: true,
-        runValidators: true,
-      })
-      .exec();
-  }
-
-  async findOneAndDelete(filter: Object): Promise<any | null> {
-    const model = await this.createModel();
-    return model.findOneAndDelete(filter).exec();
-  }
-
   async set(_id: string, newDocument: any): Promise<any | null> {
-    const model = await this.createModel();
-    return model
-      .findByIdAndUpdate(_id, newDocument, {
-        new: true,
-        runValidators: true,
-        upsert: true,
-      })
-      .exec();
+    const Model = await this.createModel();
+    return Model.findByIdAndUpdate(_id, newDocument, {
+      new: true,
+      runValidators: true,
+      upsert: true,
+    }).exec();
+  }
+
+  async delete(_id: string): Promise<any | null> {
+    const Model = await this.createModel();
+    return Model.findByIdAndDelete(_id).exec();
+  }
+
+  async validate(newDocument: any): Promise<any | null> {
+    const Model = await this.createModel();
+    const document = new Model(newDocument);
+    return document.validate();
+  }
+
+  private populate(query: Query<any>, fieldsToPopulate: string[] | QueryPopulateOptions[]): Query<any> {
+    return query.populate(fieldsToPopulate);
+  }
+
+  async getOne(filter?: Object, populateFields?: string[] | QueryPopulateOptions[]): Promise<any> {
+    const Model = await this.createModel();
+    return Model.findOne(filter).populate(populateFields).exec();
+  }
+
+  private async findOneAndUpdate(filter: Object, newDocument: any): Promise<any | null> {
+    const Model = await this.createModel();
+    return Model.findOneAndUpdate(filter, newDocument, {
+      new: true,
+      runValidators: true,
+    }).exec();
+  }
+
+  private async findOneAndDelete(filter: Object): Promise<any | null> {
+    const Model = await this.createModel();
+    return Model.findOneAndDelete(filter).exec();
   }
 }
