d08b58abc355491d2095bed4d371772d457d7d06
bolthorn91 <bolthorn91@gmail.com>
2021-11-05 17:44:05 +0000

    Add env variables to ProccessEnv namespace

--- src/types/common/process.d.ts
+++ src/types/common/process.d.ts
@@ -3,9 +3,23 @@ declare namespace NodeJS {
     MONGO_BASE_URL: string;
     MONGO_URL: string;
     MONGO_DB_NAME: string;
+    MONGO_OPTIONS: string;
     DB_USER: string;
     DB_PASSWORD: string;
     PORT: string;
     FIREBASE_URL: string;
+    MAIL: string;
+    MAIL_PASSWORD: string;
+    MODE: string;
+    AIRTABLE_KEY: string;
+    AIRTABLE_BASE: string;
+    DOMAIN_URL: string;
+    DOMAIN_API : string;
+    LOG_LEVEL: string;
+    REQUEST_LIMIT: string;
+    SESSION_SECRET: string;
+    OPENAPI_SPEC: string;
+    REDSYS_SECRET: string;
+    ACCESS_TOKEN_SECRET_PAY: string;
   }
 }
